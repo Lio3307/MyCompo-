4797cc9a7322f5f91f5f2a23b0a4fd9fd88ac095
Lio3307 <aureliofs01@gmail.com>
2025-08-10 10:09:05 +0800

    fixing: set button z-index make sure the button always on top

--- src/components/ComponentHighlight.tsx
+++ src/components/ComponentHighlight.tsx
@@ -20,6 +20,7 @@ const ComponentHighlight = ({ sourceCode, title }: ComponentProps) => {
             ? "bg-gradient-to-r from-red-500 to-red-600 hover:from-red-600 hover:to-red-700"
             : "bg-gradient-to-r from-sky-500 to-blue-600 hover:from-sky-600 hover:to-blue-700"
         } text-white`}
+         style={{zIndex:  50, position: "relative"}}
         onClick={(e) => {
           e.preventDefault();
           e.stopPropagation();
@@ -34,9 +35,10 @@ const ComponentHighlight = ({ sourceCode, title }: ComponentProps) => {
             language="html"
             maxHeight="70vh"
             outerMaxWidth="calc(100vw - 18rem)"
-          />
+          />  
       ) : (
         <div 
+        style={{pointerEvents: "none"}}
           className="bg-gradient-to-br from-slate-50 to-gray-100 rounded-2xl mb-[1rem] shadow-xl p-[0.6rem] mx-4 border border-gray-200"
           dangerouslySetInnerHTML={{ __html: sourceCode }}
         />
